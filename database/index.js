74650b864bea67f54c9cb36873c84b6857a0c59f
delder004 <dmelder@gmail.com>
2019-02-07 15:09:41 -0800

    Convert from mongo to sql schema

--- database/index.js
+++ database/index.js
@@ -1,27 +1,62 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const mysql = require('mysql');
+const mysqlConfig = require('./config.js');
 
-const bookingSchema = ({
-  booking_id: {type: Number, unique: true},
-  home_id: Number,
-  user_id: Number,
-  created_at: {type: Date, default: Date.now},
-  check_in: Date,
-  check_out: Date,
-  price_per_night: Number,
-  no_guests: Number
-});
+const connection = mysql.createConnection(mysqlConfig);
 
-const pricingSchema = ({
-  pricing_id: {type: Number, unique: true},
-  home_id: Number,
-  host_id: Number,
-  price_default: Number,
-  price_special: Number,
-  cleaning_fee: Number,
-  service_fee: Number,
-  occ_taxes: Number
-});
+const getAllLines = function(callback) {
+  const queryStr = 'SELECT * FROM `service_lines`';
+  connection.query(queryStr, (err, lines) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, lines);
+    }
+  })
+}
 
-const Booking = mongoose.model('Booking', bookingSchema);
-const Pricing = mongoose.model('Pricing', pricingSchema); 
+const getStopsById = function(lineId, callback) {
+  const queryStr = 'SELECT * FROM `stations` INNER JOIN `stops` ON stops.line_id = ? && stations.id = stops.station_id';
+  connection.query(queryStr, [lineId], (err, stops) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, stops);
+    }
+  })
+}
+
+const makeStopFav = function(stopId, callback) {
+  connection.query(
+    'SELECT stations.is_favorite FROM `stations` WHERE stations.id = ?',
+    stopId,
+    (err, favStatus) => {
+      const newStatus = (favStatus[0].is_favorite === 0) ? 1 : 0;
+      const queryStr = 'UPDATE `stations` SET stations.is_favorite = ? WHERE stations.id = ?';
+      connection.query(queryStr, [newStatus, stopId], (err) => {
+        if (err) {
+          callback(err);
+        } else {
+          callback(null);
+        }
+      });
+    }
+  );
+}
+
+const getAllStations = function(callback) {
+  const queryStr = 'SELECT * FROM `stations` ORDER BY stations.is_favorite DESC';
+  connection.query(queryStr, (err, stations) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, stations);
+    }
+  })
+}
+
+module.exports = {
+  getAllLines,
+  getStopsById,
+  makeStopFav,
+  getAllStations
+};
