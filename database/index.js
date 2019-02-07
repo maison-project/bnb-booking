bd510958e8559314b79aac0a6abbc0528ed27779
delder004 <dmelder@gmail.com>
2019-02-07 11:18:46 -0800

    Create database schemas

--- database/index.js
+++ database/index.js
@@ -1,18 +1,25 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-//create database with mongoose
+const bookingSchema = ({
+  home_id: Number,
+  user_id: Number,
+  created_at: {type: Date, default: Date.now},
+  check_in: Date,
+  check_out: Date,
+  price_per_night: Number,
+  no_guests: Number
+});
 
-//booking requests database
-  //booking id
-  //home id
-  //user id
-  //created_at
-  //check_in
-  //check_out
-  //price per night
-  //no of guests
+const pricingSchema = ({
+  home_id: Number,
+  host_id: Number,
+  price_default: Number,
+  price_special: Number,
+  cleaning_fee: Number,
+  service_fee: Number,
+  occ_taxes: Number
+});
 
-//pricing database
-  //listing id
-  //price per night
+const Booking = mongoose.model('Booking', bookingSchema);
+const Pricing = mongoose.model('Pricing', pricingSchema); 
