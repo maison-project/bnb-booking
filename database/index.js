8e2a9175a10c789d323172196e340ce8614e6d77
delder004 <dmelder@gmail.com>
2019-02-07 10:30:13 -0800

    vsc update

---
+++ database/index.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//create database with mongoose
+
+//booking requests database
+  //booking id
+  //home id
+  //user id
+  //created_at
+  //check_in
+  //check_out
+  //price per night
+  //no of guests
+
+//pricing database
+  //listing id
+  //price per night
