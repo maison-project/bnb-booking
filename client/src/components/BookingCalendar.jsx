a81be831b89facdea0c2a8d268100d1a0c0d84dd
delder004 <dmelder@gmail.com>
2019-02-08 10:23:09 -0800

    Configured webpack babel react and set up basic client components

---
+++ client/src/components/BookingCalendar.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const BookingCalendar = (props) => {
+  return (
+  );
+}
+
+export default BookingCalendar;
---
+++ client/src/sample_data.js
@@ -0,0 +1,10 @@
+const sampleBooking = {
+  booking_id: 1,
+  home_id: 1,
+  user_id: 1,
+  created_at: '2019-02-07 17:28:09',
+  check_in: '2019-03-01',
+  check_out: '2019-03-04',
+  price_per_night: 166,
+  no_guests: 2
+};
