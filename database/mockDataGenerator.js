791b41ed189ddb0815cd540d2fe155225a4052a8
delder004 <dmelder@gmail.com>
2019-02-07 19:40:14 -0800

    Add server and initial react/babel/webpack setup

--- database/mockDataGenerator.js
+++ database/mockDataGenerator.js
@@ -1,7 +1,7 @@
 for (let i = 100; i < 200; i++) {
   let user_id = Math.round(Math.random()*400 + 200);
-  let check_in = '2019-03-06'; 
-  let check_out = '2019-03-08';
+  let check_in = '2019-03-25'; 
+  let check_out = '2019-03-29';
   let price_per_night = Math.round(Math.random()*500 + 50);
   let no_guests = Math.round(Math.random()*4 + 1);
 
