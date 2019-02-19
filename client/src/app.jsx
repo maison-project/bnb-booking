f1b6d0f9e1ce838f8fb26a3ac7f11e9107feafa2
delder004 <dmelder@gmail.com>
2019-02-19 15:16:52 -0800

    Add cors to request

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -18,8 +18,9 @@ export default class App extends React.Component {
 
   getCalendar() {
     const homeId = 150;
-    fetch('localhost:3002/api/bookings/' + homeId, {
+    fetch('http://localhost:3002/api/bookings/' + homeId, {
       method: 'GET',
+      mode: 'cors',
       headers: {
         'Content-Type': 'application/json',
       },
@@ -36,8 +37,9 @@ export default class App extends React.Component {
 
   postBooking(booking) {
     console.log(`${booking} was sent`);
-    fetch('localhost:3002/api/bookings/', {
+    fetch('http://localhost:3002/api/bookings/', {
       method: 'POST',
+      mode: 'cors',
       body: JSON.stringify({booking: booking}),
       headers: {
         'Content-Type': 'application/json',
