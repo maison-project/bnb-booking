bccb8a7f6eba4e44ad65d0b98017eae3d8f5daaf
delder004 <dmelder@gmail.com>
2019-02-19 20:32:22 -0800

    Change related to proxy server

--- server/index.js
+++ server/index.js
@@ -12,6 +12,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '/../public')));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/api/bookings/:homeId', (req, res) => {
   db.getBookingsById(req.params.homeId, (err, bookings) => {
     if (err) {
