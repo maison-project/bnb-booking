53cbe13380113df2783ae85feafff6cdabca245c
Stella Zhang <stellazhang@stellazmacbook2.attlocal.net>
2019-06-18 01:42:20 -0700

    Edit database credentials

--- database/sql-config.js
+++ database/sql-config.js
@@ -1,8 +1,8 @@
 // rename as sql-config.js and modify accordingly
 
 module.exports = {
-  host: '172.17.0.2',
+  host: 'localhost',
   user: 'root',
-  password: 'booking1',
+  password: 'password',
   database: 'booking_requests',
 };
