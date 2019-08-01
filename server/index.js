a214b850e49d1395864fa89dd9d879a78f5d74d5
Stella Zhang <stellazhang@stellazmacbook2.attlocal.net>
2019-08-01 02:34:55 -0700

    Implement caching with Redis

--- server/index.js
+++ server/index.js
@@ -1,53 +1,81 @@
 const newrelic = require('newrelic');
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
-const expressStaticGzip = require('express-static-gzip');
+// const path = require('path');
+// const expressStaticGzip = require('express-static-gzip');
 
-// const db = require('../database');
-const { cal } = require('./calendarHelper');
 
+// const { cal } = require('./calendarHelper');
+const redis = require('redis');
 const db = require('../database/controller.js');
 
 const app = express();
 const PORT = 3002;
 
-app.use('/', expressStaticGzip(path.join(__dirname, '/../public'), {
-  enableBrotli: true,
-  orderPreference: ['br', 'gz'],
-}));
+const client = redis.createClient();
+
+// app.use('/', expressStaticGzip(path.join(__dirname, '/../public'), {
+//   enableBrotli: true,
+//   orderPreference: ['br', 'gz'],
+// }));
+
+
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   next();
+// });
+
+app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
+
+client.on('connect', (err) => {
+  console.log('connected to redis');
 });
 
 app.get('/api/bookings/:home_id', (req, res) => {
   const selectedHome = req.params.home_id;
-  db.getBookingsOnly(selectedHome, (err, data) => {
-    if (err) {
-      res.send('error with retrieval');
-      res.status(404);
+  client.get(selectedHome, (err, result) => {
+    if (result) {
+      res.send(JSON.parse(result));
     } else {
-      res.send(data.rows);
+      db.getBookingsOnly(selectedHome, (err, data) => {
+        if (err) {
+          res.send('error with retrieval');
+          res.status(404);
+        } else {
+          client.setex(selectedHome, 86500, JSON.stringify(data.rows));
+          res.send(data.rows);
+        }
+      });
     }
   });
 });
 
 app.get('/api/homes/:home_id', (req, res) => {
   const selectedHome = req.params.home_id;
-  db.getHomes(selectedHome, (err, data) => {
-    if (err) {
-      res.send('error retrieving home');
-      res.status(404);
+  client.get(selectedHome, (err, result) => {
+    if (result) {
+      res.send(JSON.parse(result));
     } else {
-      res.send(data.rows);
+      db.getHomes(selectedHome, (err, data) => {
+        if (err) {
+          res.send('error retrieving home');
+          res.status(404);
+        } else {
+          client.setex(selectedHome, 86500, JSON.stringify(data.rows));
+          res.send(data.rows);
+        }
+      });
     }
   });
 });
+
 // app.get('/api/bookings/:home_id', (req, res) => {
 //   const selectedHome = req.params.home_id;
 //   db.getBookings(selectedHome, (err, data) => {
@@ -70,7 +98,7 @@ app.post('/api/bookings', (req, res) => {
     } else {
       res.status(202);
       res.send('successfully booked');
-      // console.log('server receieved', results);
+      // console.log('server received', results);
     }
   });
 });
@@ -99,21 +127,4 @@ app.delete('/api/bookings', (req, res) => {
       res.send('booking deleted');
     }
   });
-});
-
-
-// app.post('/api/bookings', (req, res) => {
-//   const { booking } = req.body;
-//   // console.log(booking);
-//   db.createBooking(booking, (err) => {
-//     if (err) {
-//       // send error
-//     } else {
-//       res.send('success');
-//     }
-//   });
-// });
-
-app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`);
 });
