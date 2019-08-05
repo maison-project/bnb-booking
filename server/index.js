0d724e9c48b0a0a7afc9b8af26a1e8631fce72ea
Stella Zhang <stellazhang@stellazmacbook2.attlocal.net>
2019-08-05 00:56:28 -0700

    Implement node clustering

--- server/index.js
+++ server/index.js
@@ -6,11 +6,10 @@ const bodyParser = require('body-parser');
 
 
 // const { cal } = require('./calendarHelper');
+const cluster = require('cluster');
+const numWorkers = require('os').cpus().length;
 const redis = require('redis');
 const db = require('../database/controller.js');
-
-const app = express();
-const PORT = 3002;
 
 const client = redis.createClient();
 
@@ -25,106 +24,142 @@ const client = redis.createClient();
 //   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 //   next();
 // });
+const setupWorkerProcesses = () => {
+  console.log(`Master cluster setting up ${numWorkers} workers...`);
 
-app.use(express.static('public'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+  for (let i = 0; i < numWorkers; i++) {
+    cluster.fork();
+  }
 
-app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`);
-});
-
-client.on('connect', (err) => {
-  console.log('connected to redis');
-});
-
-app.get('/api/bookings/:home_id', (req, res) => {
-  const selectedHome = req.params.home_id;
-  client.get(selectedHome, (err, result) => {
-    if (result) {
-      res.send(JSON.parse(result));
-    } else {
-      db.getBookingsOnly(selectedHome, (err, data) => {
-        if (err) {
-          res.send('error with retrieval');
-          res.status(404);
-        } else {
-          client.setex(selectedHome, 86500, JSON.stringify(data.rows));
-          res.send(data.rows);
-        }
-      });
-    }
+  cluster.on('online', (worker) => {
+    console.log(`Worker ${worker.process.pid} is online`);
   });
-});
 
-app.get('/api/homes/:home_id', (req, res) => {
-  const selectedHome = req.params.home_id;
-  client.get(selectedHome, (err, result) => {
-    if (result) {
-      res.send(JSON.parse(result));
-    } else {
-      db.getHomes(selectedHome, (err, data) => {
-        if (err) {
-          res.send('error retrieving home');
-          res.status(404);
-        } else {
-          client.setex(selectedHome, 86500, JSON.stringify(data.rows));
-          res.send(data.rows);
-        }
-      });
-    }
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} ended with code: ${code} and signal: ${signal}`);
+    console.log('Starting new worker');
+    cluster.fork();
   });
-});
+};
 
-// app.get('/api/bookings/:home_id', (req, res) => {
-//   const selectedHome = req.params.home_id;
-//   db.getBookings(selectedHome, (err, data) => {
+if (cluster.isMaster) {
+  setupWorkerProcesses();
+} else {
+  const app = express();
+  const PORT = process.env.PORT || 3002;
+  app.use(express.static('public'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+  });
+
+  client.on('connect', (err) => {
+    console.log('connected to redis');
+  });
+
+  app.get('/api/bookings/:home_id', (req, res) => {
+    const selectedHome = req.params.home_id;
+    client.get(selectedHome, (err, result) => {
+      if (result) {
+        res.send(JSON.parse(result));
+      } else {
+        db.getBookingsOnly(selectedHome, (err, data) => {
+          if (err) {
+            res.send('error with retrieval');
+            res.status(404);
+          } else {
+            client.setex(selectedHome, 86500, JSON.stringify(data.rows));
+            res.send(data.rows);
+          }
+        });
+      }
+    });
+  });
+
+  app.get('/api/homes/:home_id', (req, res) => {
+    const selectedHome = req.params.home_id;
+    client.get(selectedHome, (err, result) => {
+      if (result) {
+        res.send(JSON.parse(result));
+      } else {
+        db.getHomes(selectedHome, (err, data) => {
+          if (err) {
+            res.send('error retrieving home');
+            res.status(404);
+          } else {
+            client.setex(selectedHome, 86500, JSON.stringify(data.rows));
+            res.send(data.rows);
+          }
+        });
+      }
+    });
+  });
+
+  // app.get('/api/bookings/:home_id', (req, res) => {
+  //   const selectedHome = req.params.home_id;
+  //   db.getBookings(selectedHome, (err, data) => {
+  //     if (err) {
+  //       res.send('error with retrieval');
+  //       res.status(404);
+  //     } else {
+  //       res.send(data.rows);
+  //     }
+  //   });
+  // });
+
+  app.post('/api/bookings', (req, res) => {
+    const data = req.body;
+    // console.log(data);
+    db.addBooking(data, (err, results) => {
+      if (err) {
+        res.status(400);
+        res.send('cannot complete booking');
+      } else {
+        res.status(202);
+        res.send('successfully booked');
+        // console.log('server received', results);
+      }
+    });
+  });
+
+
+  app.put('/api/bookings', (req, res) => {
+    const data = req.body;
+    // console.log('testing data', data);
+    db.updateBooking(data, (err, results) => {
+      if (err) {
+        res.status(400);
+        res.send('something went wrong, unable to update');
+      } else {
+        res.send('successfully updated booking');
+      }
+    });
+  });
+
+  app.delete('/api/bookings', (req, res) => {
+    const data = req.body;
+    db.deleteBooking(data, (err, results) => {
+      if (err) {
+        res.status(400);
+        res.send('unable to access booking');
+      } else {
+        res.send('booking deleted');
+      }
+    });
+  });
+}
+
+
+// app.post('/api/bookings', (req, res) => {
+//   const { booking } = req.body;
+//   // console.log(booking);
+//   db.createBooking(booking, (err) => {
 //     if (err) {
-//       res.send('error with retrieval');
-//       res.status(404);
+//       // send error
 //     } else {
-//       res.send(data.rows);
+//       res.send('success');
 //     }
 //   });
 // });
-
-app.post('/api/bookings', (req, res) => {
-  const data = req.body;
-  // console.log(data);
-  db.addBooking(data, (err, results) => {
-    if (err) {
-      res.status(400);
-      res.send('cannot complete booking');
-    } else {
-      res.status(202);
-      res.send('successfully booked');
-      // console.log('server received', results);
-    }
-  });
-});
-
-
-app.put('/api/bookings', (req, res) => {
-  const data = req.body;
-  // console.log('testing data', data);
-  db.updateBooking(data, (err, results) => {
-    if (err) {
-      res.status(400);
-      res.send('something went wrong, unable to update');
-    } else {
-      res.send('successfully updated booking');
-    }
-  });
-});
-
-app.delete('/api/bookings', (req, res) => {
-  const data = req.body;
-  db.deleteBooking(data, (err, results) => {
-    if (err) {
-      res.status(400);
-      res.send('unable to access booking');
-    } else {
-      res.send('booking deleted');
-    }
-  });
-});
